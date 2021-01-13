73df09c8c548e7b7452f9bd20c15f0803ae0fedc
Pavin Mathew <pvmathew@ucdavis.edu>
2021-01-13 13:08:09 +0900

    split into server and client sub directories

--- razzle.config.js
+++ razzle.config.js
@@ -1,3 +1,28 @@
+const path = require("path");
+const razzleHeroku = require("razzle-heroku");
+
 module.exports = {
-  modify: require("razzle-heroku"),
+  modifyWebpackConfig(opts) {
+    let config = opts.webpackConfig;
+
+    config = razzleHeroku(
+      config,
+      { target: opts.env.target, dev: opts.env.dev },
+      opts.webpackObject
+    );
+
+    return config;
+  },
+  modifyPaths({
+    webpackObject, // the imported webpack node module
+    options: {
+      razzleOptions, // the modified options passed to Razzle in the `options` key in `razzle.config.js` (options: { key: 'value'})
+    },
+    paths, // the default paths that will be used by Razzle.
+  }) {
+    paths.appServerJs = path.join(paths.appPath, "src/server/server.js");
+    paths.appServerIndexJs = path.join(paths.appPath, "src/server/index.js");
+    paths.appClientIndexJs = path.join(paths.appPath, "src/client/client.js");
+    return paths;
+  },
 };
